5e8f6f6648393b085f54a8ec9086c5e12a569bd1
umut <h.umutayran@gmail.com>
2025-03-09 17:43:25 +0300

    order service initial commit

---
+++ src/main/java/com/ayran/order_service/model/Order.java
@@ -0,0 +1,20 @@
+package com.ayran.order_service.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "t_orders")
+@Getter
+@Setter
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderNumber;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItems> orderLineItemsList;
+}
